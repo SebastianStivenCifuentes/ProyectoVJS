020767dc54928a820db3184c09c9b8c411a8b9f4
SebastianStivenCifuentes <104658606+SebastianStivenCifuentes@users.noreply.github.com>
2023-10-18 21:53:45 -0500

    Update Preguntas y respuestas.py

--- Preguntas y respuestas.py
+++ Preguntas y respuestas.py
@@ -1,29 +1,29 @@
 '''
 1
 preguntas
-    como llego al itla
+    como llego al Virrey
     cuales son las rutas de transporte
     como llego
 
 respuesta
-    El ITLA tiene 3 rutas de trasnporte la 27 de febrero, la Charles De Gaulle y la ave. Francisco Alberto Caamaño Deño
+    El Virrey tiene 3 rutas de trasnporte la 27 de febrero, la Charles De Gaulle y la ave. Francisco Alberto Caamaño Deño
 
 2
 preguntas
     en que lugar lo encuentro
-    donde esta el itla
-    donde se ubica el itla
+    donde esta el Virrey
+    donde se ubica el Virrey
 respuesta
-    El itla esta ubicado en las américas , km. 27, la caleta, calle 27, 11606
+    El Virrey esta ubicado en las américas , km. 27, la caleta, calle 27, 11606
 
 3
 preguntas
-    Cuando se fundo el itla
-    En que fecha se creo el itla
+    Cuando se fundo el Virrey
+    En que fecha se creo el Virrey
     Cual fue la fecha de su fundacion
 
 respuesta
-    El ITLA fue fundado el 15 de agosto de 2000
+    El Virrey fue fundado el 23 de agosto de 2000
 
 4
 preguntas
@@ -44,17 +44,17 @@ respuesta
 6
 preguntas
     Se fundo mediantel el Decreto No. 422-00
-    que decreto dio inicio al itla
+    que decreto dio inicio al Virrey
 respuesta
     Se fundo mediantel el Decreto No. 422-00
 
 7
 preguntas
-    cuando obtuvo el itla el ISO
-    que certificado posee el itla
+    cuando obtuvo el Virrey el ISO
+    que certificado posee el Virrey
 
 respuesta
-    El ITLA obtuvo el certificado ISO en 2000
+    El Virrey obtuvo el certificado ISO en 2000
 
 8
 preguntas
@@ -68,9 +68,9 @@ respuesta
 preguntas
     quien ocupa la rectoria
     como se llama el rector
-    quien rige el itla
+    quien rige el Virrey
 respuesta
-    Omar Méndez Lluberes es actualmente el rector del itla
+    Omar Méndez Lluberes es actualmente el rector del Virrey
 
 10
 preguntas
